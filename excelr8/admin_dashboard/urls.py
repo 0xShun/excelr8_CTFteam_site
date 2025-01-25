d008acd53125582aec70c948ea1f64b8c3184298
0xm1cx <shawnlovecode14@gmail.com>
2025-01-25 08:03:39 +0800

    Fixed the overlapping tempalte names

--- excelr8/admin_dashboard/urls.py
+++ excelr8/admin_dashboard/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_dashboard, name='admin_dashboard'),
+    path('add_team_members/', views.team_members, name='add_team_members'),
+    path('add_publications/', views.publications, name='add_publications'),
+    path('add_projects/', views.projects, name='add_projects'),
 ]
